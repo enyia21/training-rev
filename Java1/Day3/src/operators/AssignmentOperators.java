5fc0d1d8dce271ba1ac90359a54e11e0d8727044
Chibuisi Enyia <enyia21@gmail.com>
2021-07-15 12:58:24 -0500

    From Edit... to Casting DataTypes

---
+++ Java1/Day3/src/operators/AssignmentOperators.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class AssignmentOperators {
+	public static void main(String[] args) {
+		System.out.println("Is this correct!");
+		//(double) Typecasting
+		double r_whole = (double)9/2; //when dividing with int, we return a whole number;
+		System.out.println(r_whole);
+		
+	}
+}
